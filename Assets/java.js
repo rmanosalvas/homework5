47b0ea02b3479250c8be9ad8e3099ed00668e5dc
Ricardo <manosalvas2013@gmail.com>
2020-06-17 14:14:51 -0400

    reloading

--- Assets/java.js
+++ Assets/java.js
@@ -1,13 +1,14 @@
-// Setting the Current Time 
-function currentTime() {
-    var time = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $("#currentDay").html(time);
-    setTimeout(currentTime, 1000);
-}
-
 $(document).ready(function () {
+    // Setting the Current Time 
     currentTime();
-    var id = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+
+    function currentTime() {
+        var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+        $("#currentDay").html(time);
+        setTimeout(currentTime, 1000);
+    }
+
+    var id = ["#9", "#10", "#11", "#12", "#1", "#2", "#3", "#4", "#5"];
     var timeSlots = ["09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00"]
     var timeSlotsPlus = ["10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00"]
 
@@ -18,26 +19,34 @@ $(document).ready(function () {
     function getSavedData() {
         var storedData = JSON.parse(localStorage.getItem("plansArray"));
         if (storedData !== null) {
-            todoArray = storedData
+            todoArray = storedData;
         }
     };
 
     for (var i = 0; i < id.length; i++) {
-        var plansDecription = $(id[i]);
+        var plansDescription = $(id[i]);
         var currentTime = (moment().format("MMMM Do YYYY, HH:mm:ss"));
         var plannerTime = (moment().format("MMMM Do YYYY" + ", " + timeSlots[i]));
-        var plannerTimePlus = (moment().format("MMMM Do YYYY" + ", " + timeSlots[i]));
+        var timeSlotsPlus = (moment().format("MMMM Do YYYY" + ", " + timeSlotsPlus[i]));
 
         if (currentTime < plannerTime) {
-            plansDecription.attr("class", "future");
-            todoArray.forEach(function (item) {  
+            plansDescription.attr("class", "future");
+            todoArray.forEach(function (item) {
                 if (id[i] === ("#" + (item["inputID"]))) {
-                    plansDecription.val(item["inputValue"])
+                    plansDescription.val(item["inputValue"])
                 }
             })
+        } else if ((currentTime >= plannerTime) && (currentTime < timeSlotsPlus)) {
+            plansDescription.attr("class", "present");
+            plansDescription.forEach(function (item) {
+                if (id[i] === ("#" + (item["inputID"]))) {
+                    plansDescription.val(item["inputValue"])
+                }
+            });
+        } else if ((currentTime > plannerTime)) {
+            plansDescription.attr("class", "past");
+            $(".past").attr("disabled", "disabled");
         }
-
-
     }
 
 
