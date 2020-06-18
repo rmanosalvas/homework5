9c0e989056aba841bea28a2020652a9db24780fb
Ricardo <manosalvas2013@gmail.com>
2020-06-17 23:44:54 -0400

    updated java

--- Assets/java.js
+++ Assets/java.js
@@ -1,75 +1,67 @@
 $(document).ready(function () {
-    // Setting the Current Time 
+    // Setting the Current Time to display on the screen
+    // includes Month, Day, Year, Hour, Minute, Seconds (running).
     currentTime();
-
     function currentTime() {
         var time = moment().format('MMMM Do YYYY, h:mm:ss a');
         $("#currentDay").html(time);
         setTimeout(currentTime, 1000);
-    }
-
-    var id = ["#9", "#10", "#11", "#12", "#1", "#2", "#3", "#4", "#5"];
-    var timeSlots = ["09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00"]
-    var timeSlotsPlus = ["10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00"]
-
-    var todoArray = [];
-
-    getSavedData();
-
-    function getSavedData() {
-        var storedData = JSON.parse(localStorage.getItem("plansArray"));
-        if (storedData !== null) {
-            todoArray = storedData;
-        }
     };
 
-    for (var i = 0; i < id.length; i++) {
-        var plansDescription = $(id[i]);
-        var currentTime = (moment().format("MMMM Do YYYY, HH:mm:ss"));
-        var plannerTime = (moment().format("MMMM Do YYYY" + ", " + timeSlots[i]));
-        var timeSlotsPlus = (moment().format("MMMM Do YYYY" + ", " + timeSlotsPlus[i]));
 
-        if (currentTime < plannerTime) {
-            plansDescription.attr("class", "future");
-            todoArray.forEach(function (item) {
-                if (id[i] === ("#" + (item["inputID"]))) {
-                    plansDescription.val(item["inputValue"])
-                }
-            })
-        } else if ((currentTime >= plannerTime) && (currentTime < timeSlotsPlus)) {
-            plansDescription.attr("class", "present");
-            plansDescription.forEach(function (item) {
-                if (id[i] === ("#" + (item["inputID"]))) {
-                    plansDescription.val(item["inputValue"])
-                }
-            });
-        } else if ((currentTime > plannerTime)) {
-            plansDescription.attr("class", "past");
-            $(".past").attr("disabled", "disabled");
-        }
+
+    // A for loop here is running before other functions so that everything that is stored in the Local Storage is 
+    // loaded onto the page by getting the values, creating a var with the elements associated with the values ID
+    // and placing the value in the area
+    var local = Object.keys(localStorage);
+    for (let i = 0; i < local.length; i++) {
+        var value = localStorage.getItem(local[i]);
+        var dayHour = $("#" + local[i]).find("textarea")
+        dayHour.val(value);
     }
 
 
 
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        // creating a variable for the information (val()) typed in the box with the class .description
+        var value = $(this).siblings(".description").val();
+        // creating a variable for the id attribute associated with the parent element to the the button 
+        var hour = $(this).parent().attr("id");
 
-    $("button").on("click", function () {
-        event.preventDefault()
-        for (var i = 0; i < id.length; i++) {
-            var inputId = $(id[i]).attr("id");
-            var inputValue = $(id[i]).val();
-            console.log(inputId)
-            console.log(inputValue)
+        // checking vars
+        console.log(value);
+        console.log(hour);
 
-            var plansObj = {
-                "inputId": inputId,
-                "inputValue": inputValue
-            };
-            if (plansObj["inputValue"] !== "") {
-                todoArray.push(plansObj);
-            }
-            localStorage.setItem("todoArray", JSON.stringify(todoArray));
-        }
+        // setting the value and the hour associated with it to the Local Storage
+        localStorage.setItem(hour, value);
     });
 
+    function colorCode() {
+        var currentHours = moment().hours();
+
+        $(".time-block").each(function () {
+            // Grabbing the id from the selected (this) element
+            var hourEl = $(this).attr("id");
+            
+            var hourDay = hourEl.substring(hourEl.length);
+            var intHourDay = parseInt(hourDay)
+            var intCurrentHours = parseInt(currentHours);
+            if (parseInt(intHourDay) < parseInt(intCurrentHours)) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            } else if (parseInt(intHourDay) > parseInt(intCurrentHours)) {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            } else if (parseInt(intHourDay) === parseInt(intCurrentHours)) {
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+            }
+        })
+    };
+    colorCode();
 
 });
